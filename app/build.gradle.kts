308180a91e4efb05b9b6190c1ac66105dc5b3d0e
Kwensiu <kwen.siu@gmail.com>
2026-05-20 18:13:59 +0800

    chore(main): release 1.9.1 (#58)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     alias(libs.plugins.aboutlibraries)
 }
 
-private val appVersionName = "1.9.0" // x-release-please-version
+private val appVersionName = "1.9.1" // x-release-please-version
 
 private fun readGradleOrEnv(name: String): String? {
     val gradleValue = project.findProperty(name)?.toString()?.trim()
