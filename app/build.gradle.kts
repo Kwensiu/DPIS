922e426dc9b5e8fda3769ebcc43c19d65620e03f
Kwensiu <kwen.siu@gmail.com>
2026-06-13 21:59:13 +0800

    chore(main): release 1.12.2 (#82)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     alias(libs.plugins.aboutlibraries)
 }
 
-private val appVersionName = "1.12.1" // x-release-please-version
+private val appVersionName = "1.12.2" // x-release-please-version
 private val defaultNdkVersion = "28.2.13676358"
 private val defaultCmakeVersion = "3.22.1"
 
